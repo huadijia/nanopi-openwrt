aeb02938c803564e09a6396594fff4f784151314
huadijia <huadijia@qq.com>
2021-01-10 08:47:56 +0800

    update

--- 5_mods.sh
+++ 5_mods.sh
@@ -7,8 +7,9 @@ sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' 
 # echo 'wireguard' > package/base-files/files/etc/modules.d/10_wireguard
 # echo -e '\nDYC Build\n' >> package/base-files/files/etc/banner
 
-sed -i '/exit/i\echo -e "src/gz openwrt_base https://mirror.tuna.tsinghua.edu.cn/openwrt/releases/19.07.1/packages/aarch64_cortex-a53/base" > /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
-sed -i '/exit/i\echo -e "src/gz openwrt_luci https://mirror.tuna.tsinghua.edu.cn/openwrt/releases/19.07.1/packages/aarch64_cortex-a53/luci" >> /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
-sed -i '/exit/i\echo -e "src/gz openwrt_packages https://mirror.tuna.tsinghua.edu.cn/openwrt/releases/19.07.1/packages/aarch64_cortex-a53/packages" >> /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
-sed -i '/exit/i\echo -e "src/gz openwrt_routing https://mirror.tuna.tsinghua.edu.cn/openwrt/releases/19.07.1/packages/aarch64_cortex-a53/routing" >> /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
-sed -i '/exit/i\echo -e "src/gz openwrt_telephony https://mirror.tuna.tsinghua.edu.cn/openwrt/releases/19.07.1/packages/aarch64_cortex-a53/telephony" >> /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit/i\echo -e "src/gz openwrt_base https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/base" > /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit/i\echo -e "src/gz openwrt_luci https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/luci" >> /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit/i\echo -e "src/gz openwrt_packages https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/packages" >> /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit/i\echo -e "src/gz openwrt_routing https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/routing" >> /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit/i\echo -e "src/gz openwrt_telephony https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/telephony" >> /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit/i\echo -e "src/gz openwrt_freifunk https://downloads.openwrt.org/snapshots/packages/aarch64_cortex-a53/freifunk" >> /etc/opkg/distfeeds.conf' package/lean/default-settings/files/zzz-default-settings
